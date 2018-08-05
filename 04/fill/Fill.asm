f4ebe07fc83003f3eb1eb6119e70586ad9d340a8
Deep-Red <kirbycolebrown@gmail.com>
2018-08-05 03:18:27 -0400

    Fill first attempt.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,37 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BEGIN)
+@SCREEN
+D=A
+@R2
+M=D
+@KEYBOARD
+D=M
+@R3
+M=D
+(LOOP)
+ @R2
+ D=M
+ @CLEAR
+ M;JEQ
+ A=-1
+ M=M+1
+ @LOOP
+ D;JNE
+ @BEGIN
+ D;JEQ
+ (CLEAR)
+ @R2
+ A=0
+ M=M+1
+ @LOOP
+ D;JEQ
+ @BEGIN
+ D;JNE
+(END)
