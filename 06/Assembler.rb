0d19bb35e11773f0bfa9b6a5d5ab1ea2aa3a9ddd
Deep-Red <kirbycolebrown@gmail.com>
2018-08-16 00:10:41 -0400

    Refactor for clarity and strip unwanted lines from input.

--- 06/Assembler.rb
+++ 06/Assembler.rb
@@ -1,7 +1,21 @@
+# Use argument as assembly file to process
 asm_file = ARGV[0]
+
+# Extract path for output file
 path = /^.*\./.match(asm_file)
+
+# Debug Line
 puts asm_file
-puts path
+
+# Setup for input and output
 asm = IO.readlines(asm_file)
 hack = File.open("#{path}hack", "w")
+
+# Strip comments and black lines from instructions
+asm.select! { |inst| /\S/ =~ inst && /^(\/\/).*/ !~ inst}
+
+asm.each { |instruction|
+
+}
+
 hack.close
