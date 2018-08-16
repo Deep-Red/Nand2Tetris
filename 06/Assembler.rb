619e8cd0e7c2e74c8929d8cd2d6d84147a38943b
Deep-Red <kirbycolebrown@gmail.com>
2018-08-16 01:41:44 -0400

    Build A instructions and set Jump bits for C instructions.

--- 06/Assembler.rb
+++ 06/Assembler.rb
@@ -12,10 +12,41 @@ asm = IO.readlines(asm_file)
 hack = File.open("#{path}hack", "w")
 
 # Strip comments and black lines from instructions
-asm.select! { |inst| /\S/ =~ inst && /^(\/\/).*/ !~ inst}
+asm.select! { |line| /\S/ =~ line && /^(\/\/).*/ !~ line}
 
-asm.each { |instruction|
+# Sets last three bits for jump condition
+def set_jump_cond(cond)
+  case cond
+  when "JGT"
+    "001"
+  when "JEQ"
+    "010"
+  when "JGE"
+    "011"
+  when "JLT"
+    "100"
+  when "JNE"
+    "101"
+  when "JLE"
+    "110"
+  when "JMP"
+    "111"
+  end
+end
 
+# Builds instructions
+asm.each { |asm_inst|
+  puts asm_inst
+  puts asm_inst[0]
+  if asm_inst[0] == "@"
+    instruction = asm_inst[1..-1].to_i.to_s(2).rjust(16, "0")
+  elsif /;/ =~ asm_inst
+    instruction[13..14] = set_jump_cond(asm_inst[-3..-1])
+    instruction[]
+  else
+    instruction = asm_inst
+  end
+  hack.puts instruction
 }
 
 hack.close
